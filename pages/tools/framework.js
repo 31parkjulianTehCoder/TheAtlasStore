770afddbee2f5f4b3bbe35dc53126a0c1660a810
31parkjulianTehCoder <105089696+31parkjulianTehCoder@users.noreply.github.com>
2026-01-28 11:32:30 +0900

    Add framework.js with utility functions and classes

---
+++ pages/tools/framework.js
@@ -0,0 +1,77 @@
+
+//framework thingy
+function $(i){ return document.getElementById(i) }
+function loadScript(x){
+    let i = document.createElement("script");
+    i.src = x;
+    i.async = true;
+    document.head.appendChild(i);
+}
+class OSInfo {
+    constructor(name, creator, releaseDate, previewLink, Github, License, Website){
+        this.name = name;
+        this.creator = creator;
+        this.releaseDate = releaseDate;
+        this.previewLink = previewLink;
+        this.Github = Github;
+        this.License = License;
+        this.Website = Website
+    }
+}
+class AppInfo {
+    constructor(name, creator, desc, links){
+        this.name = name;
+        this.creator = creator;
+        this.desc = desc;
+        this.links = links;
+    }
+}
+const make = {
+    target: null,
+    setTarget(i){
+        this.target = i
+    },
+    newElement(t, i, p = {}){
+        let m = document.createElement(t);
+        if (p.className) {
+            m.classList.add(...p.className.split(" "));
+        }
+        if (p.id) m.id = p.id;
+        if (p.src) m.src = p.src;
+        if (p.styles) m.style = p.styles;
+        if (p.onClick) m.onClick = p.onClick;
+        m.textContent = i;
+        if (this.target === null){
+            this.setTarget(document.body)
+        }
+        this.target.appendChild(m)
+    },
+    h1(i, p){
+        this.newElement("h1", i, p);
+    },
+    h2(i, p){
+        this.newElement("h2", i, p);
+    },
+    h3(i, p){
+        this.newElement("h3", i, p);
+    },
+    p(i, p){
+        this.newElement("p", i, p);
+    },
+    div(i, p){
+        this.newElement("div", i, p);
+    },
+    script(i, p){
+        this.newElement("script", i, p);
+    },
+    style(i, p){
+        this.newElement("style", i, p);
+    },
+    iframe(i, p){
+        this.newElement("iframe", i, p);
+    },
+    button(i, p){
+        this.newElement("button", i, p)
+    },
+};
+
