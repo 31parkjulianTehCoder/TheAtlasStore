10878d35f50b1a699e8a224e9b291a4f268ee290
31parkjulianTehCoder <105089696+31parkjulianTehCoder@users.noreply.github.com>
2026-01-29 14:50:26 -0800

    Update navbar-UI.js

--- tools/navbar-UI.js
+++ tools/navbar-UI.js
@@ -1,7 +1,7 @@
 make.div("", { id: "navbar", styles: " height: 50px; background-color: lightgray; display: flex; justify-content: space-between;" });
 make.setTarget($("navbar"));
 make.h2("Atlas")
-make.a("OSes", { href: "/TheAtlasStore/tools/404.html" });
-make.a("Apps", { href: "/TheAtlasStore/tools/404.html" });
-make.a("Extentions", { href: "/TheAtlasStore/tools/404.html" });
-make.a("Blog", { href: "/TheAtlasStore/tools/404.html" });
+make.a("OSes", { href: "/TheAtlasStore/tools/-410.html" });
+make.a("Apps", { href: "/TheAtlasStore/tools/-410.html" });
+make.a("Extentions", { href: "/TheAtlasStore/tools/-410.html" });
+make.a("Blog", { href: "/TheAtlasStore/tools/-410.html" });
